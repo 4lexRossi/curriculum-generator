841d6687f07db700a15b7034f6614bbeca47db48
Alexsander Osmir Rossi <devalexrossi@gmail.com>
2021-09-04 22:33:59 -0300

    Creating First Route to homeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { HomeModule } from './modules/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'alos-cv-generator',
+    pathMatch: 'full'
+  },
+  {
+    path: 'alos-cv-generator',
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HomeModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
