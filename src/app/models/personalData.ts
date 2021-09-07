f8f01c4704f7d077524058513e3c9d157fe7493d
Alexsander Osmir Rossi <devalexrossi@gmail.com>
2021-09-07 15:12:32 -0300

    Personal Form complete

--- src/app/models/personalData.ts
+++ src/app/models/personalData.ts
@@ -6,4 +6,12 @@ export interface PersonalData {
     address: string;
     zipCode: number | null;
     city: string;
+    birthDay: string;
+    birthMonth: string;
+    birthYear: string;
+    citizenship: string;
+    gender: string;
+    civilState: string;
+    linkedIn: string;
+    website: string;
 }
--- src/app/shared/components/stepper/tabs/personal-form/personal-form.component.ts
+++ src/app/shared/components/stepper/tabs/personal-form/personal-form.component.ts
@@ -8,11 +8,14 @@ import { PersonalData } from 'src/app/models/personalData';
   templateUrl: './personal-form.component.html',
   styleUrls: ['./personal-form.component.scss']
 })
-export class PersonalFormComponent implements OnInit {
+export class PersonalFormComponent {
+
   languages: Languages[] = [
     {value: 'english-0', viewValue: 'English'},
     {value: 'portuguese-1', viewValue: 'Portuguese'}
   ];
+
+  days = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
   selectedLanguage = this.languages[1].value;
   constructor(private readonly formBuilder: FormBuilder,) { }
@@ -24,7 +27,15 @@ export class PersonalFormComponent implements OnInit {
     telephone: null,
     address: '',
     zipCode: null,
-    city: ''
+    city: '',
+    birthDay: '',
+    birthMonth: '',
+    birthYear: '',
+    citizenship: '',
+    civilState: '',
+    gender: '',
+    linkedIn: '',
+    website: ''
   }
 
   fields = {
@@ -32,9 +43,15 @@ export class PersonalFormComponent implements OnInit {
     lastName : new FormControl(this.personalData.lastName, [Validators.required, Validators.maxLength(30)]),
     email : new FormControl(this.personalData.email, [Validators.required, Validators.email]),
     telephone : new FormControl(this.personalData.telephone, [Validators.required, Validators.pattern('^\\d+'), Validators.maxLength(18)]),
-    address : new FormControl(this.personalData.address, [Validators.maxLength(30)]),
+    address : new FormControl(this.personalData.address, [Validators.maxLength(250)]),
     zipCode : new FormControl(this.personalData.zipCode, [Validators.pattern('^\\d+'), Validators.maxLength(18)]),
     city : new FormControl(this.personalData.city, [Validators.maxLength(30)]),
+    birthDate : new FormControl(this.personalData.birthDay + this.personalData.birthMonth + this.personalData.birthYear),
+    citizenship : new FormControl(this.personalData.city, [Validators.maxLength(30)]),
+    civilState : new FormControl(this.personalData.civilState, [Validators.maxLength(30)]),
+    gender : new FormControl(this.personalData.gender),
+    linkedIn : new FormControl(this.personalData.linkedIn, [Validators.maxLength(30)]),
+    website : new FormControl(this.personalData.website, [Validators.maxLength(30)]),
   }
 
   personalDataForm = this.formBuilder.group({
@@ -45,10 +62,13 @@ export class PersonalFormComponent implements OnInit {
     address : this.fields.address,
     zipCode : this.fields.zipCode,
     city : this.fields.city,
+    birthDate : this.fields.birthDate,
+    citizenship : this.fields.citizenship,
+    civilState : this.fields.civilState,
+    gender : this.fields.gender,
+    linkedIn : this.fields.linkedIn,
+    website : this.fields.website
   });
-
-  ngOnInit(): void {
-  }
 
   errorMessages = {
     required:'This Field is required',
