11ec5d1a0b7f9e286478a8f9fe737cd6521075d9
Alexsander Osmir Rossi <devalexrossi@gmail.com>
2021-09-13 21:36:48 -0300

    Format files identation

--- src/app/modules/generate-form/generate-form.component.ts
+++ src/app/modules/generate-form/generate-form.component.ts
@@ -2,24 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-generate-form',
-  templateUrl: './generate-form.component.html',
-  styleUrls: ['./generate-form.component.scss']
+	selector: 'app-generate-form',
+	templateUrl: './generate-form.component.html',
+	styleUrls: ['./generate-form.component.scss']
 })
 export class GenerateFormComponent implements OnInit {
-  firstFormGroup!: FormGroup;
-  secondFormGroup!: FormGroup;
-  reviewFormGroup!: FormGroup;
+	firstFormGroup!: FormGroup;
+	secondFormGroup!: FormGroup;
+	reviewFormGroup!: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) { }
+	constructor(private _formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
-    this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required]
-    });
-    this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required]
-    });
-  }
+	ngOnInit(): void {
+		this.firstFormGroup = this._formBuilder.group({
+			firstCtrl: ['', Validators.required]
+		});
+		this.secondFormGroup = this._formBuilder.group({
+			secondCtrl: ['', Validators.required]
+		});
+	}
 
 }
